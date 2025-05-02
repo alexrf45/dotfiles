cc5f63cb25d39ee80140c8e4fb2f68467389604d
alexrf45 <fonalex45@gmail.com>
2025-05-02 10:35:11 -0400

    laptop set up

--- .zsh/tmux_default.sh
+++ .zsh/tmux_default.sh
@@ -2,7 +2,7 @@
 
 tmux_default() {
   if command -v tmux &>/dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
-    exec tmuxp load ~/.config/tmuxp/debian-vm.yaml
+    exec tmuxp load ~/.config/tmuxp/debian-desktop.yaml
   fi
 }
 
