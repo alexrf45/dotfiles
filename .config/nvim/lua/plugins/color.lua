a98ca1b24ccde57b18536674112b0b62fd4d24e6
alexrf45 <fonalex45@gmail.com>
2025-06-26 12:46:09 -0400

    htb theme again

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,9 +1,9 @@
 return {
-  "nyoom-engineering/oxocarbon.nvim",
+  "audibleblink/hackthebox.vim",
   lazy = false,
   priority = 1000,
   config = function()
     vim.opt.background = "dark"
-    vim.cmd.colorscheme("oxocarbon")
+    vim.cmd.colorscheme("hackthebox")
   end,
 }
