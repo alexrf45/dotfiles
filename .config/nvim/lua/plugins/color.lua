4f2b1f5e6b8b48f9b69b85d052efccce91b57a50
alexrf45 <fonalex45@gmail.com>
2025-07-09 14:22:45 -0400

    gruvbox dark-hard

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,46 +1,8 @@
--- -- Lazy
--- return {
---   "vague2k/vague.nvim",
---   lazy = false,
---   priority = 1000,
---   config = function()
---     require("vague").setup({
---       colors = {
---         bg = "#141415",
---         fg = "#cdcdcd",
---         floatBorder = "#878787",
---         line = "#252530",
---         comment = "#606079",
---         builtin = "#b4d4cf",
---         func = "#c48282",
---         string = "#e8b589",
---         number = "#e0a363",
---         property = "#c3c3d5",
---         constant = "#aeaed1",
---         parameter = "#bb9dbd",
---         visual = "#333738",
---         error = "#d8647e",
---         warning = "#f3be7c",
---         hint = "#7e98e8",
---         operator = "#90a0b5",
---         keyword = "#6e94b2",
---         type = "#9bb4bc",
---         search = "#405065",
---         plus = "#7fa563",
---         delta = "#f3be7c",
---       },
---     })
---     vim.cmd.colorscheme("vague")
---   end,
---
---
--- }
-
 return {
   "RRethy/base16-nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("base16-gruvbox-material-dark-soft")
+    vim.cmd.colorscheme("base16-gruvbox-material-dark-hard")
   end,
 }
