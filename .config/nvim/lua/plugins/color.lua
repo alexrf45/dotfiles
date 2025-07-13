58e52925297c3c79fc3c8509016095ccfbc53767
alexrf45 <fonalex45@gmail.com>
2025-07-13 16:27:09 -0400

    irblack

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,12 +1,21 @@
+-- return {
+--   "sainnhe/gruvbox-material",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     -- Optionally configure and load the colorscheme
+--     -- directly inside the plugin declaration.
+--     vim.g.gruvbox_material_enable_italic = true
+--     vim.g.gruvbox_material_background = "hard"
+--     vim.cmd.colorscheme("gruvbox-material")
+--   end,
+-- }
+--
 return {
-  "sainnhe/gruvbox-material",
+  "RRethy/base16-nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    -- Optionally configure and load the colorscheme
-    -- directly inside the plugin declaration.
-    vim.g.gruvbox_material_enable_italic = true
-    vim.g.gruvbox_material_background = "hard"
-    vim.cmd.colorscheme("gruvbox-material")
+    vim.cmd.colorscheme("base16-irblack")
   end,
 }
