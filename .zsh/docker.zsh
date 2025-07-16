ee87e41de49a5b62ca861d1d5436193b6dfdeec6
alexrf45 <fonalex45@gmail.com>
2025-07-16 13:38:22 -0400

    aliases sep files

---
+++ .zsh/docker.zsh
@@ -0,0 +1,35 @@
+#docker
+alias d='docker'
+alias db='docker build'
+alias dimls='docker image ls'
+alias dim='docker image'
+alias dc='docker container'
+alias dnt='docker network'
+
+#unprivileged docker shell
+alias ds='docker run --rm -v "$(pwd):$(pwd)" -w "$(pwd)" -u "$(id -u):$(id -g)" -it'
+
+#docker compose
+alias d-up='docker-compose up -d'
+alias d-down='docker-compose down'
+
+#wipe all docker resources
+alias dnuke="docker stop $(docker ps -a -q); docker rm -f $(docker ps -a -q); docker rmi -f $(docker images -a -q); docker volume rm -f $(docker volume ls -q)"
+
+#lazydocker
+alias lzd='docker run --rm -it -v /var/run/docker.sock:/var/run/docker.sock -v /yourpath:/.config/jesseduffield/lazydocker lazyteam/lazydocker'
+
+#portainer
+alias portainer='docker run --name portainer -p 9000:9000 -d -v "/var/run/docker.sock:/var/run/docker.sock" portainer/portainer-ce:latest'
+alias portainerstop='docker stop portainer'
+alias portainerstart='docker start portainer'
+
+#gcp docker cli
+alias gcloud='docker run --rm --volumes-from gcloud-config gcr.io/google.com/cloudsdktool/google-cloud-cli gcloud'
+
+
+#juiceshop docker container
+alias juiceshop='docker run --name juiceshop -d --rm -p 3000:3000 bkimminich/juice-shop'
+
+#terraform-docs
+alias tf-docs='docker run --rm --volume "$(pwd):/terraform-docs" -u $(id -u) quay.io/terraform-docs/terraform-docs:0.20.0 markdown /terraform-docs'
---
+++ .zsh/kube.zsh
@@ -0,0 +1,13 @@
+alias context='kubectx'
+alias k-shell='kubecolor run tmp-shell --rm -i --tty --image nicolaka/netshoot -- /bin/bash'
+alias cluster='kubecolor config get-clusters'
+alias switch='kubectx'
+alias k='kubecolor'
+alias ka='kubecolor apply'
+alias kaf='kubectl apply -f'
+alias kd='kubectl delete'
+alias kg='kubecolor get'
+alias kgn='kubecolor get nodes'
+alias kgp='kubecolor get pods'
+
+
