060b06b15fc6c83a6d4a7b7ef937ea963d227d05
Sean Fontaine <fonalex45@gmail.com>
2025-07-19 17:49:39 -0400

    mac theme

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,21 +1,21 @@
-return {
-  "sainnhe/gruvbox-material",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    -- Optionally configure and load the colorscheme
-    -- directly inside the plugin declaration.
-    vim.g.gruvbox_material_enable_italic = true
-    vim.g.gruvbox_material_background = "soft"
-    vim.cmd.colorscheme("gruvbox-material")
-  end,
-}
---
 -- return {
---   "RRethy/base16-nvim",
+--   "sainnhe/gruvbox-material",
 --   lazy = false,
 --   priority = 1000,
 --   config = function()
---     vim.cmd.colorscheme("base16-irblack")
+--     -- Optionally configure and load the colorscheme
+--     -- directly inside the plugin declaration.
+--     vim.g.gruvbox_material_enable_italic = true
+--     vim.g.gruvbox_material_background = "soft"
+--     vim.cmd.colorscheme("gruvbox-material")
 --   end,
 -- }
+--
+return {
+  "RRethy/base16-nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme("base16-macintosh")
+  end,
+}
