375b456be03f2bb6ef5281a8875ea4189b276370
Sean Fontaine <fonalex45@gmail.com>
2025-07-21 15:36:50 -0400

    rusty theme

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -20,12 +20,42 @@
 --   end,
 -- }
 
+-- return {
+--   "fcancelinha/nordern.nvim",
+--   branch = "master",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     vim.cmd.colorscheme("nordern")
+--   end,
+-- }
+
 return {
-  "fcancelinha/nordern.nvim",
-  branch = "master",
+  "armannikoyan/rusty",
   lazy = false,
   priority = 1000,
-  config = function()
-    vim.cmd.colorscheme("nordern")
+  opts = {
+    transparent = true,
+    italic_comments = true,
+    underline_current_line = true,
+    colors = {
+      foreground = "#c5c8c6",
+      background = "#1d1f21",
+      selection = "#373b41",
+      line = "#282a2e",
+      comment = "#969896",
+      red = "#cc6666",
+      orange = "#de935f",
+      yellow = "#f0c674",
+      green = "#b5bd68",
+      aqua = "#8abeb7",
+      blue = "#81a2be",
+      purple = "#b294bb",
+      window = "#4d5057",
+    },
+  },
+  config = function(_, opts)
+    require("rusty").setup(opts)
+    vim.cmd("colorscheme rusty")
   end,
 }
