c26b2490c5467e82b9502ef4abe0dff3d36a3e63
alexrf45 <fonalex45@gmail.com>
2025-08-24 10:05:44 -0400

    onedark-dark

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,29 +1,17 @@
--- return {
---   "loctvl842/monokai-pro.nvim",
---   lazy = false,
---   priority = 1000,
---   config = function()
---     require("monokai-pro").setup({
---       transparent_background = true,
---     })
---     vim.cmd("colorscheme monokai-pro-spectrum")
---   end,
--- }
-
--- return {
---   "RRethy/base16-nvim",
---   lazy = false,
---   priority = 1000,
---   config = function()
---     vim.cmd.colorscheme("base16-gruvbox-material-dark-hard")
---   end,
--- }
-
 return {
-  "rmehri01/onenord.nvim",
+  "RRethy/base16-nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("onenord")
+    vim.cmd.colorscheme("base16-onedark-dark")
   end,
 }
+
+-- return {
+--   "rmehri01/onenord.nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     vim.cmd.colorscheme("onenord")
+--   end,
+-- }
