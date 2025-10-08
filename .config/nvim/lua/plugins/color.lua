87be3d13912a78926c7fd5e1e97290b7702c30bb
alexrf45 <fonalex45@gmail.com>
2025-10-08 17:47:14 -0400

    nordic

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,26 +1,9 @@
 return {
-  "everviolet/nvim",
-  name = "evergarden",
-  priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
-  opts = {
-    theme = {
-      variant = "fall", -- 'winter'|'fall'|'spring'|'summer'
-      accent = "red",
-    },
-    editor = {
-      transparent_background = false,
-      sign = { color = "none" },
-      float = {
-        color = "mantle",
-        solid_border = false,
-      },
-      completion = {
-        color = "surface0",
-      },
-    },
-  },
+  "AlexvZyl/nordic.nvim",
+  lazy = false,
+  priority = 1000,
   config = function()
-    vim.cmd.colorscheme("evergarden")
+    require("nordic").load()
   end,
 }
 
