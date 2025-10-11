e14aa3f7559f983d28cc4eb006df82deeb726a55
alexrf45 <fonalex45@gmail.com>
2025-10-11 04:23:52 -0400

    bright

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -1,44 +1,26 @@
 return {
-  "AlexvZyl/nordic.nvim",
+  "RRethy/base16-nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    require("nordic").load()
+    vim.cmd.colorscheme("base16-bright")
+    require("base16-colorscheme").setup({
+      base00 = "#000000", -- Default background (Tango black)
+      base01 = "#555753", -- Lighter background (Tango bright black)
+      base02 = "#555753", -- Selection background
+      base03 = "#888a85", -- Comments, line numbers
+      base04 = "#babdb6", -- Dark foreground
+      base05 = "#d3d7cf", -- Default foreground (Tango white)
+      base06 = "#eeeeec", -- Light foreground (Tango bright white)
+      base07 = "#eeeeec", -- Light background
+      base08 = "#ef2929", -- Red (variables, errors)
+      base09 = "#fcaf3e", -- Orange (integers, constants)
+      base0A = "#fce94f", -- Yellow (classes, search)
+      base0B = "#8ae234", -- Green (strings)
+      base0C = "#34e2e2", -- Cyan (regex, support)
+      base0D = "#729fcf", -- Blue (functions, methods)
+      base0E = "#ad7fa8", -- Magenta (keywords)
+      base0F = "#c4a000", -- Brown (deprecated)
+    })
   end,
 }
-
--- return {
---   "vague2k/vague.nvim",
---   lazy = false, -- make sure we load this during startup if it is your main colorscheme
---   priority = 1000, -- make sure to load this before all the other plugins
---   config = function()
---     require("vague").setup({
---       colors = {
---         bg = "#000000",
---         inactiveBg = "#1c1c24",
---         fg = "#cdcdcd",
---         floatBorder = "#878787",
---         line = "#252530",
---         comment = "#606079",
---         builtin = "#b4d4cf",
---         func = "#c48282",
---         string = "#e8b589",
---         number = "#e0a363",
---         property = "#c3c3d5",
---         constant = "#aeaed1",
---         parameter = "#bb9dbd",
---         visual = "#333738",
---         error = "#d8647e",
---         warning = "#f3be7c",
---         hint = "#7e98e8",
---         operator = "#90a0b5",
---         keyword = "#6e94b2",
---         type = "#9bb4bc",
---         search = "#405065",
---         plus = "#7fa563",
---         delta = "#f3be7c",
---       },
---     })
---     vim.cmd("colorscheme vague")
---   end,
--- }
