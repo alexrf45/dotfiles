3a3462a67b2f5f8e6c3c5c7ad4af44f763ccd01d
alexrf45 <fonalex45@gmail.com>
2026-05-15 15:46:50 -0400

    new theme

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -3,7 +3,7 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("base16-terracotta-dark")
+    vim.cmd.colorscheme("base16-synth-midnight-dark")
     --
   end,
 }
