3cfb2837b2d64859be1ddf534c979fbd05483d2f
alexrf45 <fonalex45@gmail.com>
2026-05-22 14:34:04 -0400

    gruv

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -3,7 +3,7 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("base16-black-metal-venom")
+    vim.cmd.colorscheme("base16-gruvbox-material-dark-soft")
     --
   end,
 }
