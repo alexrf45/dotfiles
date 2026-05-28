937aa24b83ffd1dd517b960251826d2a8f5e2d0e
alexrf45 <fonalex45@gmail.com>
2026-05-28 02:03:19 -0400

    markdown

--- .config/nvim/lua/plugins/color.lua
+++ .config/nvim/lua/plugins/color.lua
@@ -3,7 +3,7 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("base16-catppuccin")
+    vim.cmd.colorscheme("base16-gruvbox-material-dark-soft")
     --
   end,
 }
